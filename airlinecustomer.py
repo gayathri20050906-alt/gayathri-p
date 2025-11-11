2feba21b44157ef144958ebbba93a6fa2c7549d3
gayathri20050906-alt <gayathri20050906@gmail.com>
2025-11-11 10:39:12 +0530

    Add files via upload

---
+++ airlinecustomer.py
@@ -0,0 +1,42 @@
+import numpy as np
+import pandas as pd
+df=pd.read_csv(r"C:\Users\Gayat\Downloads\Airline_Customerproject.csv")
+print(df)
+
+
+df.info()
+
+df(df.head)
+
+print(df.describe)
+
+print(df.drop_duplicates)
+
+print(df.__dataframe__)
+
+print(df.columns)
+
+from sqlalchemy import create_engine
+username="root"
+password="root"
+host="localhost"
+port="3306"
+database="airline"
+
+engine=create_engine(f"mysql+pymysql://root:root@localhost:3306/airline")
+table_name="airlinesprint"
+df.to_sql(table_name,engine,if_exists='replace',index=False)
+ 
+print("frist 5 rowsof data")
+print(df.head())
+
+print("\ndata info:")
+print(df.info)
+
+print("\nMissing values in each column:")
+print(df.isnull().sum())
+
+
+
+filtered_loyal = df[(df['Customer_Type'] == 'Loyal Customer') & (df['Class'] == 'Business')]
+print(filtered_loyal.head())
